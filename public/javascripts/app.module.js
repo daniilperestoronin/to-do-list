18e0059b3de83909bd0f964f94c62471797894e7
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-03-20 16:22:21 +0300

    added angular app

---
+++ public/javascripts/app.module.js
@@ -0,0 +1,43 @@
+(function () {
+    angular.module('ToDoListApp', [])
+
+        .controller('ToDoListCtrl', function ($scope) {
+
+            $scope.tasks = [
+                {
+                    name: 'Example task 1',
+                    done: false
+                },
+                {
+                    name: 'Example task 2',
+                    done: false
+                }
+            ]
+
+            $scope.createdTask = {
+                name: null
+            };
+
+            $scope.progress =0;
+
+            $scope.newTask = function () {
+                if ($scope.createdTask.name) {
+                    $scope.tasks.push({name: $scope.createdTask.name, done: false});
+                    console.log('added new task');
+                } else {
+                    alert('Enter file name!');
+                }
+            }
+
+            $scope.checkProgress = function () {
+                var all= $scope.tasks.length;
+                console.log(all);
+                var done = $scope.tasks.filter(function(element) {
+                    return element.done==true;
+                }).length
+                console.log(done);
+                $scope.progress = done/all*100;
+            }
+
+        });
+})();
