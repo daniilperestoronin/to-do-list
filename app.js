fcbb81c30e3b1d63a20a4d45dd264fd860425ddd
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-03-24 12:57:51 +0300

    Change ejs on html

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, './web/views'));
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
