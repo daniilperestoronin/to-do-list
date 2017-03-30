b333ea726bbf1a6c6356797e3f288809f3b0879f
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-03-30 12:38:49 +0300

    Added tasksList

--- web/src/javascripts/components/todolist/todolist.js
+++ web/src/javascripts/components/todolist/todolist.js
@@ -3,16 +3,40 @@
 
         .controller('ToDoListCtrl', function ($scope) {
 
-            $scope.tasks = [
+            $scope.tasksList = [
                 {
-                    name: 'Example task 1',
-                    done: false
+                    name: 'Lists 1',
+                    active: true,
+                    done: false,
+                    tasks: [
+                        {
+                            name: 'Lists 1 Example task 1',
+                            done: false
+                        },
+                        {
+                            name: 'Lists 1 Example task 2',
+                            done: false
+                        }
+                    ]
                 },
                 {
-                    name: 'Example task 2',
-                    done: false
+                    name: 'Lists 2',
+                    active: false,
+                    done: false,
+                    tasks: [
+                        {
+                            name: 'Lists 2 Example task 1',
+                            done: false
+                        },
+                        {
+                            name: 'Lists 2 Example task 2',
+                            done: false
+                        }
+                    ]
                 }
-            ]
+            ];
+
+            $scope.tasks = [];
 
             $scope.createdTask = {
                 name: null
@@ -28,7 +52,7 @@
                 } else {
                     alert('Enter file name!');
                 }
-            }
+            };
 
             $scope.checkProgress = function () {
                 var all = $scope.tasks.length;
@@ -38,24 +62,28 @@
                 }).length
                 console.log(done);
                 $scope.progress = done / all * 100;
-            }
+            };
 
             $scope.changeProgress = function () {
                 if ($scope.progress != 0) {
                     $scope.checkProgress();
                 }
-            }
+            };
 
             $scope.clearAllTasks = function () {
                 $scope.tasks = [];
                 $scope.progress = 0;
-            }
+            };
 
             $scope.deleteTask = function (task) {
                 var index = $scope.tasks.indexOf(task);
                 $scope.tasks.splice(index, 1);
-                $scope.changeProgress()
-            }
+                $scope.changeProgress();
+            };
 
+            $scope.changeActiveTaskList = function (list) {
+                list.active = true;
+                $scope.tasks = list.tasks;
+            };
         });
 })();
