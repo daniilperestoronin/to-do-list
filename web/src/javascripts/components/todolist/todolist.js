783ddd0192a838f8ed54a40badfee20c966886fb
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-03-31 17:27:37 +0300

    Added list progress for all lists

--- web/src/javascripts/components/todolist/todolist.js
+++ web/src/javascripts/components/todolist/todolist.js
@@ -47,26 +47,10 @@
             $scope.newTask = function () {
                 if ($scope.createdTask.name) {
                     $scope.tasks.push({name: $scope.createdTask.name, done: false});
-                    $scope.changeProgress()
+                    $scope.progress = $scope.changeProgress($scope.tasks);
                     console.log('added new task');
                 } else {
                     alert('Enter file name!');
-                }
-            };
-
-            $scope.checkProgress = function () {
-                var all = $scope.tasks.length;
-                console.log(all);
-                var done = $scope.tasks.filter(function (element) {
-                    return element.done == true;
-                }).length
-                console.log(done);
-                $scope.progress = done / all * 100;
-            };
-
-            $scope.changeProgress = function () {
-                if ($scope.progress != 0) {
-                    $scope.checkProgress();
                 }
             };
 
@@ -78,12 +62,27 @@
             $scope.deleteTask = function (task) {
                 var index = $scope.tasks.indexOf(task);
                 $scope.tasks.splice(index, 1);
-                $scope.changeProgress();
+                $scope.progress = $scope.changeProgress($scope.tasks);
             };
 
             $scope.changeActiveTaskList = function (list) {
                 list.active = true;
                 $scope.tasks = list.tasks;
+                $scope.progress = $scope.changeProgress(list.tasks);
+            };
+
+            $scope.checkProgress = function () {
+                $scope.progress = $scope.changeProgress($scope.tasks);
+            }
+
+            $scope.changeProgress = function (tasks) {
+                var all = tasks.length;
+                console.log(all);
+                var done = tasks.filter(function (element) {
+                    return element.done == true;
+                }).length
+                console.log(done);
+                return done / all * 100;
             };
         });
 })();
