f3f34834ad52bdf9600b37ed3a0b8137575d7ef1
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-05-10 22:22:24 +0300

    Added greeting page style and content

--- web/src/scripts/app.module.js
+++ web/src/scripts/app.module.js
@@ -39,7 +39,7 @@
                 $rootScope.$on('$locationChangeStart', function (event, next, current) {
                     // redirect to login page if not logged in
                     if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
-                        $location.path('/login');
+                        $location.path('/');
                     }
                 });
             }]);
