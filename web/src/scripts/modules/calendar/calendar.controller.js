17aabe7eeb92cadc65e2431c4880d5984679c1ff
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-05-19 17:32:49 +0300

    Added events in calendar

--- web/src/scripts/modules/calendar/calendar.controller.js
+++ web/src/scripts/modules/calendar/calendar.controller.js
@@ -4,7 +4,31 @@
     angular.module('Calendar')
         .controller('CalendarController',
             function ($scope) {
-                /* config object */
+
+                var date = new Date();
+                var d = date.getDate();
+                var m = date.getMonth();
+                var y = date.getFullYear();
+
+                $scope.events = [
+                    {title: 'All Day Event', start: new Date(y, m, 1)},
+                    {title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2)},
+                    {id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false},
+                    {id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false},
+                    {
+                        title: 'Birthday Party',
+                        start: new Date(y, m, d + 1, 19, 0),
+                        end: new Date(y, m, d + 1, 22, 30),
+                        allDay: false
+                    },
+                    {
+                        title: 'Click for Google',
+                        start: new Date(y, m, 28),
+                        end: new Date(y, m, 29),
+                        url: 'http://google.com/'
+                    }
+                ];
+
                 $scope.uiConfig = {
                     calendar: {
                         height: 450,
@@ -17,5 +41,7 @@
                         defaultDate: new Date()
                     }
                 };
+
+                $scope.eventSources = [$scope.events]
             });
 })();
