cb0d17f9531662b4eebbe6537101b79e9a23ea21
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-05-22 18:35:48 +0300

    Added calendar service

--- web/src/scripts/modules/calendar/calendar.controller.js
+++ web/src/scripts/modules/calendar/calendar.controller.js
@@ -2,36 +2,14 @@
     'use strict';
 
     angular.module('Calendar')
-        .controller('CalendarController',
-            function ($scope) {
+        .controller('CalendarController', ['$scope', 'CalendarService',
+            function ($scope, CalendarService) {
 
-                var date = new Date();
-                var d = date.getDate();
-                var m = date.getMonth();
-                var y = date.getFullYear();
-
-                $scope.events = [
-                    {title: 'All Day Event', start: new Date(y, m, 1)},
-                    {title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2)},
-                    {id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false},
-                    {id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false},
-                    {
-                        title: 'Birthday Party',
-                        start: new Date(y, m, d + 1, 19, 0),
-                        end: new Date(y, m, d + 1, 22, 30),
-                        allDay: false
-                    },
-                    {
-                        title: 'Click for Google',
-                        start: new Date(y, m, 28),
-                        end: new Date(y, m, 29),
-                        url: 'http://google.com/'
-                    }
-                ];
+                $scope.events = CalendarService.getAllEvents();
 
                 $scope.uiConfig = {
                     calendar: {
-                        height: 450,
+                        height: 800,
                         editable: true,
                         header: {
                             left: 'title',
@@ -41,7 +19,6 @@
                         defaultDate: new Date()
                     }
                 };
-
                 $scope.eventSources = [$scope.events]
-            });
+            }]);
 })();
---
+++ web/src/scripts/modules/calendar/calendar.service.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular.module('Calendar')
+
+        .factory('CalendarService', ['ToDoListService', function (ToDoListService) {
+
+            var tasksList = ToDoListService.getAllTaskLists();
+
+            var service = {};
+
+            service.getAllEvents = getAllEvents;
+
+            return service;
+
+            function getAllEvents() {
+                var tasksEvents = [];
+                for (var i = 0; i < tasksList.length; i++) {
+                    for (var j = 0; j < tasksList[i].tasks.length; j++) {
+                        tasksEvents.push({
+                            title: tasksList[i].tasks[j].name,
+                            start: tasksList[i].tasks[j].startDate,
+                            end: tasksList[i].tasks[j].endDate
+                        })
+                    }
+                }
+                return tasksEvents;
+            };
+        }]);
+})();
